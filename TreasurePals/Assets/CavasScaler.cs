1059a24de5ed20813783e962d558bfaa6de57d65
Danny <dny.ngyn@gmail.com>
2016-09-28 17:27:21 -0700

    danny pushing frame design

---
+++ TreasurePals/Assets/CavasScaler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CavasScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<CanvasScaler> ().referenceResolution = new Vector2 (Screen.width, Screen.height);
+	}
+	/*
+	void Awake(){
+		GetComponent<CanvasScaler> ().referenceResolution = new Vector2 (Screen.width, Screen.height);
+
+	}*/
+
+}
